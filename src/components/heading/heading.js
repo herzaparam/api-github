b25d2ceefc95d6cd1d53868a40537a25829aa42a
herzaparam <herzaparam@gmail.com>
2021-06-16 11:20:06 +0700

    add functionality

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './heading.module.css'
 
 function Heading() {
+
+    const [name, setName] = useState("")
+
+    const handleClick = () => {
+        console.log(name);
+    }
+
     return (
         <div className={styles.container}>
             <h5>Search :</h5>
-            <input type="text" placeholder="type here" />
+            <input type="text" placeholder="type here" onChange={(e) => setName(e.target.value)} />
+            <button onClick={handleClick}>Search</button>
         </div>
     )
 }
